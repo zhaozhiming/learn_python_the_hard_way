74f200581e144423ebc910d85009c40889d7cdbe
zhaozhiming <kingzzm1982@sina.com>
2015-08-11 17:49:15 +0800

    ex20

---
+++ ex20.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from sys import argv
+
+script, input_file = argv
+
+
+def print_all(f):
+    print f.read()
+
+
+def rewind(f):
+    f.seek(0)
+
+
+def print_a_line(line_count, f):
+    print line_count, f.readline()
+
+
+current_file = open(input_file)
+
+print "First let's print the whole file:\n"
+
+print_all(current_file)
+
+print "Now let's rewind, kind of like a tape."
+
+rewind(current_file)
+
+print "Let's print three lines:"
+
+for i in range(3):
+    print_a_line(i + 1, current_file)
